47c2e7fffe5c31d4c6aec101d4f3071839323da3
easunt <kdgiant174@gmail.com>
2021-12-09 23:57:34 +0900

    CREATE - Capability findAll, findOne 기능

--- src/main/kotlin/com/perfume/jomalone/default/entity/Capability.kt
+++ src/main/kotlin/com/perfume/jomalone/default/entity/Capability.kt
@@ -1,16 +1,19 @@
 package com.perfume.jomalone.default.entity
 
 import com.perfume.jomalone.common.entity.BaseEntity
+import org.springframework.data.jpa.repository.JpaRepository
 import javax.persistence.*
 
 @Entity
 class Capability(
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long = 0L,
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long = 0L,
     val name: String,
     @OneToMany(fetch = FetchType.EAGER)
     @JoinColumn(name = "capability_id")
     val capabilityAttributes: List<CapabilityAttribute>
-) : BaseEntity() {
+) : BaseEntity() {}
+
+interface CapabilityRepository : JpaRepository<Capability, Long> {
+
 }
+
---
+++ src/main/kotlin/com/perfume/jomalone/default/model/CapabilityRequest.kt
@@ -0,0 +1,6 @@
+package com.perfume.jomalone.default.model
+
+data class CapabilityRequest(
+    val id: Long,
+    val name: String
+)
